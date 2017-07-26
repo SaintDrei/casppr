65e1fa3db99a3cd9013133cabd3160750389c1da
SaintDrei <andreimishael.santos@globalpay.com>
2017-07-26 18:33:50 +0800

    'commitment'

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+            </div>
+        </div>
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Made by <a href="#">Santos, Andrei Mishael</a></p>
+                </div>
+            </div>
+        </footer>
+    </div>
+     <!--Import jQuery before materialize.js-->
+    
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    
+    <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+    <script src="script.js"></script>  
+
+</body>
+</html>
