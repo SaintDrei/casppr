96ff314c5f8f83de1fe836efaccffd95bed4d870
SaintDrei <andreimishael.santos@globalpay.com>
2017-07-28 19:08:14 +0800

    registration form added input fields

--- account/register.php
+++ account/register.php
@@ -3,16 +3,16 @@
 	include_once('../includes/header.php');
 ?>
     <div class="row">
-        <div class="col s12 m10 l8 push-l2">
+        <div class="col s12 m10 l8 push-l2 push-m1">
             <div class="card">
                 <div class="card-image">
                     <img src="../content/images/spot/logo.jpg" height="300em">
                 </div>
                 <div class="row">
                 <div class="card-content">
-                    <form class="col s12 m10 l10">
+                    <form class="col s12 m12 l10">
                         <div class="row">
-                            <div class="input-field col s12 m6">
+                            <div class="input-field col s12 m6 l6 push-l1">
                                 <select class="icons">
                                   <option value="" disabled selected>Choose your option</option>
                                   <option value="" data-icon="../content/images/coro/logo.jpg" class="left circle">Coro San Benildo</option>
@@ -24,8 +24,35 @@
                                 </select>
                                 <label>Artist Group</label>
                             </div>
+                            <div class="input-field col s12 m6 l6 push-l1">
+                                <select class="icons">
+                                  <option value="" disabled selected>Choose your option</option>
+                                  <?php echo $_subgroups ?>
+                                    
+                                </select>
+                                <label>Artist Group</label>
+                            </div>
                         </div>
-                    
+                        <div class="row">
+                            <div class="input-field col s12 l6 m6 push-l1">
+                                <input id="SID" type="number" class="validate">
+                                <label for="schoolID">School ID</label>
+                            </div>
+                        <div class="input-field col s12 l6 m6 push-l1">
+                                <input id="username" disabled type="text" class="validate">
+                                <label for="username">Username</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12 l6 m6 push-l1">
+                                <input id="FN" type="number" class="validate">
+                                <label for="FN">First Name</label>
+                            </div>
+                        <div class="input-field col s12 l6 m6 push-l1">
+                                <input id="LN" type="text" class="validate">
+                                <label for="LN">Last Name</label>
+                            </div>
+                        </div>
                     
                     </form>
                 </div>
