7e21c98e25db0473915521e97b5030525b62c960
SaintDrei <andreimishael.santos@globalpay.com>
2017-07-29 17:40:38 +0800

    rest

--- account/register.php
+++ account/register.php
@@ -15,7 +15,10 @@
 		
 	}
 
+
+
 ?>
+<div class="row"></div><div class="row"></div>
     <div class="row">
         <div class="col s12 m10 l8 push-l2 push-m1">
             <div class="card">
@@ -27,7 +30,7 @@
                     <form class="col s12 m12 l10">
                         <div class="row">
                             <div class="input-field col s12 m6 l6 push-l1">
-                                <select class="icons">
+                                <select class="icons" tabindex="1">
                                   <option value="" disabled selected>Choose your Artist Group</option>
                                   <option value="" data-icon="../content/images/coro/logo.jpg" class="left circle">Coro San Benildo</option>
                                   <option value="" data-icon="../content/images/cpt/logo.png" class="left circle">Cultural Promotions Team</option>
@@ -39,7 +42,7 @@
                                 <label>Artist Group</label>
                             </div>
                             <div class="input-field col s12 m6 l6 push-l1">
-                                <select class="icons">
+                                <select class="icons" tabindex="2">
                                   <option value="" disabled selected>Choose your option</option>
                                   <?php echo $_subgroups ?>
                                     
@@ -49,78 +52,86 @@
                         </div>
                          <div class="row">
                             <div class="input-field col s12 l6 m6 push-l1">
-                                <input id="FN" type="number" class="validate">
+                                <input id="FN" type="number" class="validate" tabindex="3">
                                 <label for="FN">First Name</label>
                             </div>
                         <div class="input-field col s12 l6 m6 push-l1">
-                                <input id="LN" type="text" class="validate">
+                                <input id="LN" type="text" class="validate" tabindex="4">
                                 <label for="LN">Last Name</label>
                             </div>
                         </div>
                         <div class="row">
                             
                         <div class="input-field col s12 l6 m6 push-l1">
-                                <input id="username" disabled type="text" class="validate">
+                                <input id="username" disabled type="text" class="validate" tabindex="5">
                                 <label for="username">Username</label>
                             </div>
                             <div class="input-field col s12 l6 m6 push-l1">
-                                <input id="password" type="password" class="validate">
+                                <input id="password" type="password" class="validate" tabindex="6">
                                 <label for="password">Password</label>
                             </div>
                         </div>
                         <div class="row">
                             
                         <div class="input-field col s12 l6 m6 push-l1">
-                                <input id="email" type="email" class="validate">
+                                <input id="email" type="email" class="validate" tabindex="7">
                                 <label for="email">E-Mail</label>
                             </div>
                             <div class="input-field col s12 l6 m6 push-l1">
-                                <input id="SID" type="number" class="validate" maxlength="11">
+                                <input id="SID" type="number" class="validate" maxlength="11"  tabindex="8">
                                 <label for="SID">School ID</label>
                             </div>
                         </div> 
                         <div class="row">    
                         <div class="input-field col s12 l6 m6 push-l1">
-                                <input id="mobile" type="number" class="validate" maxlength="11">
+                                <input id="mobile" type="number" class="validate" maxlength="11" tabindex="9">
                                 <label for="mobile">Mobile</label>
                             </div>
                             <div class="input-field col s12 l6 m6 push-l1">
-                                <input id="landline" type="number" class="validate" maxlength="7">
+                                <input id="landline" type="number" class="validate" maxlength="7" tabindex="10">
                                 <label for="landline">Landline</label>
                             </div>
                         </div>
                      <div class="row">    
                         <div class="input-field col s12 l12 m12 push-l1">
-                                <input id="address" type="text" class="validate">
+                                <input id="address" type="text" class="validate" tabindex="11">
                                 <label for="address">Address</label>
                             </div>
                                                   
                         </div>
                      <div class="row">    
                         <div class="input-field col s12 l6 m6 push-l1">
-                                <input id="zip" type="text" class="validate">
+                                <input id="zip" type="text" class="validate" tabindex="12">
                                 <label for="zip">Zip</label>
                             </div>
                           <div class="input-field col s12 l6 m6 push-l1">
-                                <input id="city" type="text" class="validate">
+                                <input id="city" type="text" class="validate" tabindex="13">
                                 <label for="city">City</label>
                             </div>
                             
                         </div>
                       <div class="row">    
                         <div class="input-field col s12 l6 m6 push-l1">
-                             <input type="text" class="datepicker" id="birthdate">
+                             <input type="text" class="datepicker" id="birthdate" tabindex="14">
                             <label for="birthdate">Birthday</label>
                             </div>
                          <div class="input-field col s12 m6 l6 push-l1">
-                                <select>
+                                <select tabindex="15">
                                     <option value="" disabled selected>Scholarship</option>
                                     <?php echo $list_scholar; ?> 
                              </select>
                          </div>
                             
                         </div>
-                     
+                     <div class="row">
+                            <div class="input-field col s12">
+                              <button class="btn cyan waves-effect waves-light right" type="submit" name="action">Submit
+                                <i class="tiny material-icons right">send</i>
+                              </button>
+                                
+                            </div>
+                        </div>
+                        <div class="row"></div>
                     
                     </form>
                 </div>
